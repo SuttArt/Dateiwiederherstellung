70c031f12213f6f730a47f46cd995695d5e2cb4e
Artem Suttar <artem.suttar@gmail.com>
2024-12-06 22:53:39 +0100

    Fix BGD Offset again. Add tmp Solution for BGD

--- src/ext2.rs
+++ src/ext2.rs
@@ -1,4 +1,3 @@
-
 // implementation template for convenience
 // see https://www.nongnu.org/ext2-doc/ext2.html for documentation on ext2 fs
 
@@ -8,23 +7,23 @@ use crate::ext2;
 
 #[derive(Debug)]
 pub struct Ext2FS {
-	super_block: Superblock,
+    super_block: Superblock,
     // The block group descriptor table is an array of block group descriptor, used to define parameters of all the block groups.
     block_group_descriptors: Vec<BlockGroupDescriptor>,
 }
 
 pub struct BlockIter {
-	// TODO: fill this structure
+    // TODO: fill this structure
 }
 
 #[derive(Debug, Clone, Copy)]
 #[repr(C, packed)]
 struct Superblock {
-	inodes_count: u32,
-	blocks_count: u32,
-	block_size: u32,
+    inodes_count: u32,
+    blocks_count: u32,
+    block_size: u32,
     blocks_per_group: u32,
-	rev_level: u32,
+    rev_level: u32,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -37,30 +36,30 @@ struct BlockGroupDescriptor {
     bg_free_inodes_count: u16,
     bg_used_dirs_count: u16,
     bg_pad: u16,
-    bg_reserved: u128,
+    bg_reserved: [u8; 12],
 }
 
 impl Superblock {
-	fn new(block: &[u8]) -> Self {
-		// Parse relevant fields from the Superblock
-		let inodes_count = u32::from_le_bytes(block[0..4].try_into().unwrap());
-		let blocks_count = u32::from_le_bytes(block[4..8].try_into().unwrap());
-		let log_block_size = u32::from_le_bytes(block[24..28].try_into().unwrap());
+    fn new(block: &[u8]) -> Self {
+        // Parse relevant fields from the Superblock
+        let inodes_count = u32::from_le_bytes(block[0..4].try_into().unwrap());
+        let blocks_count = u32::from_le_bytes(block[4..8].try_into().unwrap());
+        let log_block_size = u32::from_le_bytes(block[24..28].try_into().unwrap());
         let blocks_per_group = u32::from_le_bytes(block[32..36].try_into().unwrap());
-		let rev_level = u32::from_le_bytes(block[76..80].try_into().unwrap());
+        let rev_level = u32::from_le_bytes(block[76..80].try_into().unwrap());
 
-		// Calculate the block size#
-		// The block size is computed using this 32bit value as the number of bits to shift left the value 1024. This value may only be non-negative.
-		let block_size = 1024 << log_block_size;
+        // Calculate the block size#
+        // The block size is computed using this 32bit value as the number of bits to shift left the value 1024. This value may only be non-negative.
+        let block_size = 1024 << log_block_size;
 
-		Superblock {
-			inodes_count,
-			blocks_count,
-			block_size,
+        Superblock {
+            inodes_count,
+            blocks_count,
+            block_size,
             blocks_per_group,
-			rev_level
-		}
-	}
+            rev_level,
+        }
+    }
 
     // Add getter methods to safely access fields
     fn inodes_count(&self) -> u32 {
@@ -79,20 +78,36 @@ impl Superblock {
     fn rev_level(&self) -> u32 {
         self.rev_level
     }
+
+    fn print_parsed_info(&self) {
+        // Print the parsed Superblock information
+        println!("Inodes Count: {}", self.inodes_count());
+        println!("Blocks Count: {}", self.blocks_count());
+        println!("Block Size: {} bytes", self.block_size());
+        println!("Blocks per group: {}", self.blocks_per_group());
+        println!("Revision Level: {}", self.rev_level());
+    }
 }
 
 impl BlockGroupDescriptor {
-	fn new(data: &[u8]) -> Self {
-        let bg_block_bitmap =  u32::from_le_bytes(data[0..4].try_into().unwrap());
-        let bg_inode_bitmap =  u32::from_le_bytes(data[4..8].try_into().unwrap());
-        let bg_inode_table =  u32::from_le_bytes(data[8..12].try_into().unwrap());
-        let bg_free_blocks_count =  u16::from_le_bytes(data[12..14].try_into().unwrap());
-        let bg_free_inodes_count =  u16::from_le_bytes(data[14..16].try_into().unwrap());
-        let bg_used_dirs_count =  u16::from_le_bytes(data[16..18].try_into().unwrap());
-        let bg_pad =  u16::from_le_bytes(data[18..20].try_into().unwrap());
-        let bg_reserved =  u128::from_le_bytes(data[20..32].try_into().unwrap());
+    // Temporary(?) solution, because I don't want to store invalid BGDs, and while
+    // bg_block_bitmap == 0 is invalid - we will filter it out
+    fn new(data: &[u8]) -> Option<Self> {
+        let bg_block_bitmap = u32::from_le_bytes(data[0..4].try_into().unwrap());
+        let bg_inode_bitmap = u32::from_le_bytes(data[4..8].try_into().unwrap());
+        let bg_inode_table = u32::from_le_bytes(data[8..12].try_into().unwrap());
+        let bg_free_blocks_count = u16::from_le_bytes(data[12..14].try_into().unwrap());
+        let bg_free_inodes_count = u16::from_le_bytes(data[14..16].try_into().unwrap());
+        let bg_used_dirs_count = u16::from_le_bytes(data[16..18].try_into().unwrap());
+        let bg_pad = u16::from_le_bytes(data[18..20].try_into().unwrap());
+        let bg_reserved = data[20..32].try_into().unwrap();
 
-        BlockGroupDescriptor {
+        // Check if bg_block_bitmap is invalid
+        if bg_block_bitmap == 0 {
+            return None; // Return None if the block bitmap is invalid
+        }
+
+        Some(BlockGroupDescriptor {
             bg_block_bitmap,
             bg_inode_bitmap,
             bg_inode_table,
@@ -101,43 +116,65 @@ impl BlockGroupDescriptor {
             bg_used_dirs_count,
             bg_pad,
             bg_reserved,
-        }
+        })
+    }
+
+    fn bg_block_bitmap(&self) -> u32 { self.bg_block_bitmap }
+    fn bg_inode_bitmap(&self) -> u32 { self.bg_inode_bitmap }
+    fn bg_inode_table(&self) -> u32 { self.bg_inode_table }
+    fn bg_free_blocks_count(&self) -> u16 { self.bg_free_blocks_count }
+    fn bg_free_inodes_count(&self) -> u16 { self.bg_free_inodes_count }
+    fn bg_used_dirs_count(&self) -> u16 { self.bg_used_dirs_count }
+    fn bg_pad(&self) -> u16 { self.bg_pad }
+    fn bg_reserved(&self) -> &[u8; 12] { &self.bg_reserved }
+
+    pub fn print_parsed_info(&self) {
+        // Print the parsed Block Group Descriptor information
+        println!("Block Bitmap Block: {}", self.bg_block_bitmap());
+        println!("Inode Bitmap Block: {}", self.bg_inode_bitmap());
+        println!("Inode Table Block: {}", self.bg_inode_table());
+        println!("Free Blocks Count: {}", self.bg_free_blocks_count());
+        println!("Free Inodes Count: {}", self.bg_free_inodes_count());
+        println!("Used Directories Count: {}", self.bg_used_dirs_count());
+        println!("Padding: {}", self.bg_pad());
+        println!("Reserved: {:?}", self.bg_reserved()); // Print reserved as a byte array
     }
 }
 
 impl Ext2FS {
-	pub fn new(mut _device: fs::File) -> io::Result<Ext2FS> {
-		// Seek to the Superblock location (offset 1024 bytes)
-		_device.seek(SeekFrom::Start(1024))?;
+    pub fn new(mut _device: fs::File) -> io::Result<Ext2FS> {
+        // Seek to the Superblock location (offset 1024 bytes)
+        _device.seek(SeekFrom::Start(1024))?;
 
-		// Read 1024 bytes (size of the Superblock)
-		let mut buffer = [0u8; 1024];
-		_device.read_exact(&mut buffer)?;
-		let superblock = Superblock::new(&buffer);
+        // Read 1024 bytes (size of the Superblock)
+        let mut buffer = [0u8; 1024];
+        _device.read_exact(&mut buffer)?;
+        let superblock = Superblock::new(&buffer);
 
-        // Print the parsed Superblock information
-        println!("Inodes Count: {}", superblock.inodes_count());
-        println!("Blocks Count: {}", superblock.blocks_count());
-        println!("Block Size: {} bytes", superblock.block_size());
-        println!("Blocks per group: {}", superblock.blocks_per_group());
-        println!("Revision Level: {}", superblock.rev_level());
+        superblock.print_parsed_info();
 
         // Get BlockGroupDescriptor
         let block_size = superblock.block_size();
         // The block group descriptor table starts on the first block following the superblock.
         // This would be the third block on a 1KiB block file system, or the second block for 2KiB and larger block file systems.
         let descriptor_table_offset = if block_size > 1024 {
-            2 * block_size // Descriptor table is in block 2 if block size > 1024
+            block_size // Descriptor table is in block 2 if block size > 1024
         } else {
-            3 * block_size // Otherwise, it's in block 3
+            2 * block_size // Otherwise, it's in block 3
         };
 
-		println!("Descriptor Table Offset: {}", descriptor_table_offset);
+        println!("Descriptor Table Offset: {}", descriptor_table_offset);
         // Depending on how many block groups are defined, this table can require multiple blocks of storage. Always refer to the superblock in case of doubt.
         // Calculate the number of block groups
-        let blocks_per_group = superblock.blocks_count() / superblock.blocks_per_group();
+
+        // For now, because I see this:
+        // Block count:              2048
+        // Blocks per group:         8192
+        // And it's make no sense for me, how can I calculate block_groups_number
+        // I assume, that Block Group Descriptor Table can not span more than 1 block (in fact, according to the documentation, that's not true.)
+        let block_groups_number = block_size / 32; //superblock.blocks_count() / superblock.blocks_per_group();
         // Each Block Group Descriptor is 32 bytes in size.
-        let mut buffer = vec![0u8; 32 * blocks_per_group as usize];
+        let mut buffer = vec![0u8; 32 * block_groups_number as usize];
 
         println!("blocks_per_group: {}", superblock.blocks_count());
 
@@ -148,57 +185,38 @@ impl Ext2FS {
         let mut block_group_descriptors: Vec<BlockGroupDescriptor> = vec![];
 
         // Parse each Block Group Descriptor
-        for i in 0..blocks_per_group {
+        for i in 0..block_groups_number {
             let start = (i as usize) * 32;
             let end = start + 32;
-            let descriptor = BlockGroupDescriptor::new(&buffer[start..end]);
-            block_group_descriptors.push(descriptor);
+            if let Some(descriptor) = BlockGroupDescriptor::new(&buffer[start..end]) {
+                block_group_descriptors.push(descriptor);
+            } else {
+                // Red text: "\x1b[31m ...  \x1b[0m"
+                println!("\x1b[31mInvalid Block Bitmap Block in Block Group {}\x1b[0m", i);
+                break; // Exit the loop if an invalid descriptor is encountered
+            }
         }
 
-		Ok(
+        for (i, descriptor) in block_group_descriptors.iter().enumerate() {
+            println!("Block Group {}:", i);
+            descriptor.print_parsed_info();
+            println!();
+        }
+
+        Ok(
             Ext2FS {
                 super_block: superblock,
-                block_group_descriptors
-		}
+                block_group_descriptors,
+            }
         )
-	}
-
-    pub fn read_block_group_descriptors(&mut self, file: &mut std::fs::File) -> std::io::Result<()> {
-        let block_size = self.super_block.block_size() as u64;
-        // The block group descriptor table starts on the first block following the superblock.
-        // This would be the third block on a 1KiB block file system, or the second block for 2KiB and larger block file systems.
-        let descriptor_table_offset = if block_size > 1024 {
-            block_size // Descriptor table is in block 1 if block size > 1024
-        } else {
-            1024 + block_size// Otherwise, it's in block 2
-        };
-
-        // Depending on how many block groups are defined, this table can require multiple blocks of storage.
-        // Calculate the number of block groups
-        let blocks_per_group = self.super_block.blocks_count / self.super_block.blocks_per_group;
-        let mut buffer = vec![0u8; 32 * blocks_per_group as usize];
-
-        // Read the Block Group Descriptor Table
-        file.seek(SeekFrom::Start(descriptor_table_offset))?;
-        file.read_exact(&mut buffer)?;
-
-        // Parse each Block Group Descriptor
-        for i in 0..blocks_per_group {
-            let start = (i as usize) * 32;
-            let end = start + 32;
-            let descriptor = BlockGroupDescriptor::new(&buffer[start..end]);
-            // self.block_group_descriptors.push(descriptor);
-        }
-
-        Ok(())
     }
 }
 
 impl Iterator for BlockIter {
-	type Item = (); 
-	
-	fn next(&mut self) -> Option<()> {
-		// TODO: implement an iterator over unused blocks
-		None
-	}
+    type Item = ();
+
+    fn next(&mut self) -> Option<()> {
+        // TODO: implement an iterator over unused blocks
+        None
+    }
 }
